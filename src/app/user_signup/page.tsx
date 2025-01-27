234a0be71302a92ede16c7c4ef902fca09327f05
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2025-01-28 00:56:46 +0545

    change in dashboard page and public folder and adding tsconnfig file.

--- src/app/user_signup/page.tsx
+++ src/app/user_signup/page.tsx
@@ -2,7 +2,9 @@
 
 import React, { useState } from "react";
 import Image from "next/image"; 
-import { FiEye, FiEyeOff } from "react-icons/fi"; 
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
+
 
 const UserSignup = () => {
   const [form, setForm] = useState({
